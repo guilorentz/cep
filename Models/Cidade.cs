c5eb7b1a4e057ad4840199cea7efaa09e35d0db4
guilorentz <go.lorentz@gmail.com>
2021-11-19 12:41:32 -0300

    initial commit

---
+++ Models/Cidade.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CEP.Models
+{   
+    [Table("Cidades")]
+    public class Cidade
+    {
+        [Display(Name = "Id")]
+        [Column("Id")]
+        public int Id { get; set; }
+        [Display(Name = "Nome")]
+        [Column("Nome")]
+        public string Nome { get; set; }
+    }
+}
